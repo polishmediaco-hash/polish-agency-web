f18c4ce1beb7372d5ec45972acb61d18dd5812d6
POLISH Media Co <contact@polishmediaco.com>
2026-09-05 00:52:13 +0100

    feat(server): register /palette-preview route

--- server/index.js
+++ server/index.js
@@ -71,6 +71,10 @@ app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/admin.html'));
 });
 
+app.get('/palette-preview', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/palette-preview.html'));
+});
+
 // 404 Fallback
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, '../public/index.html'));

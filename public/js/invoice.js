f97396cfea410971c302b144aac10b4348132668
POLISH Media Co <contact@polishmediaco.com>
2026-09-13 03:52:20 +0100

    fix(invoice): close footer div tags properly

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -423,6 +423,8 @@
             <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
             <bdi>${state.payableEmail}</bdi>
           </span>
+        </div>
+      </footer>
     `;
 
     // Re-bind contenteditable listeners
